7505985be747713014cdfa1e7ba52d56bdeda209
dwerkjem <derekrneilson@gmail.com>
2024-03-20 11:18:23 +0000

    removed lsp zero we will use a different plugin for that

--- lua/nvd/packer.lua
+++ lua/nvd/packer.lua
@@ -28,22 +28,6 @@ use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
 use 'mbbill/undotree'
 use('tpope/vim-fugitive')
 use('github/copilot.vim')
-use {
-  'VonHeikemen/lsp-zero.nvim',
-  branch = 'v3.x',
-  requires = {
-    --- Uncomment the two plugins below if you want to manage the language servers from neovim
-    -- {'williamboman/mason.nvim'},
-    -- {'williamboman/mason-lspconfig.nvim'},
-
-    -- LSP Support
-    {'neovim/nvim-lspconfig'},
-    -- Autocompletion
-    {'hrsh7th/nvim-cmp'},
-    {'hrsh7th/cmp-nvim-lsp'},
-    {'L3MON4D3/LuaSnip'},
-  }
-}
 use "nvim-lua/plenary.nvim" -- don't forget to add this one if you don't have it yet!
 use {
     "ThePrimeagen/harpoon",
