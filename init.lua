a7213aaf2182b13fd9ef23cf20d5e13846d92296
dwerkjem <derekrneilson@gmail.com>
2024-03-20 08:21:59 +0000

    made some changes tips.txt will only be printed if it is not empty

--- init.lua
+++ init.lua
@@ -1,7 +1,12 @@
 require("nvd.config")
 
--- print the contents of tips.txt
+-- print the contents of tips.txt if and only if it is not empty
 local file = io.open("tips.txt", "r")
-print(file:read("*a"))
-file:close()
+if file then
+    local contents = file:read("*a")
+    if contents ~= "" then
+	print(contents)
+    end
+    file:close()
+end
 
