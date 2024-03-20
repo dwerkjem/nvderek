59062ecfbd141f72a534eca4a97b09e32ab5e2c9
dwerkjem <derekrneilson@gmail.com>
2024-03-20 12:35:22 +0000

    added auto-closing brackets and quotes etc

--- lua/nvd/packer.lua
+++ lua/nvd/packer.lua
@@ -58,13 +58,19 @@ return require("packer").startup(function(use)
 			require("conform").setup()
 		end,
 	})
-	use {
-        "hrsh7th/nvim-cmp",
-        requires = {
-            "hrsh7th/cmp-buffer", "hrsh7th/cmp-nvim-lsp",
-            'quangnguyen30192/cmp-nvim-ultisnips', 'hrsh7th/cmp-nvim-lua',
-            'octaltree/cmp-look', 'hrsh7th/cmp-path', 'hrsh7th/cmp-calc',
-            'f3fora/cmp-spell', 'hrsh7th/cmp-emoji'
-        }
-    }
+	use({
+		"hrsh7th/nvim-cmp",
+		requires = {
+			"hrsh7th/cmp-buffer",
+			"hrsh7th/cmp-nvim-lsp",
+			"quangnguyen30192/cmp-nvim-ultisnips",
+			"hrsh7th/cmp-nvim-lua",
+			"octaltree/cmp-look",
+			"hrsh7th/cmp-path",
+			"hrsh7th/cmp-calc",
+			"f3fora/cmp-spell",
+			"hrsh7th/cmp-emoji",
+		},
+	})
+	use("m4xshen/autoclose.nvim")
 end)
