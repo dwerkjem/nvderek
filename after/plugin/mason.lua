1a99f6920a6c6cec141247ac4ebc7e3fbdd11478
dwerkjem <derekrneilson@gmail.com>
2024-03-20 21:22:45 -0600

    diff --git a/after/plugin/mason.lua b/after/plugin/mason.lua

--- after/plugin/mason.lua
+++ after/plugin/mason.lua
@@ -2,4 +2,7 @@ require("mason").setup()
 require("mason-lspconfig").setup()
 vim.keymap.set("n", "<leader>om", "<cmd>Mason<cr>")
 
-
+require("mason-lspconfig").setup({
+	ensure_installed = { "lua_ls", "rust_analyzer" },
+	automatic_install = true,
+})
