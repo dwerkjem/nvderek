fe1a2f525e6f2b90b01077b287f98cf097339352
dwerkjem <derekrneilson@gmail.com>
2024-03-20 22:27:47 -0600

    updated

--- after/plugin/mason.lua
+++
@@ -1,8 +0,0 @@
-require("mason").setup()
-require("mason-lspconfig").setup()
-vim.keymap.set("n", "<leader>om", "<cmd>Mason<cr>")
-
-require("mason-lspconfig").setup({
-	ensure_installed = { "lua_ls", "rust_analyzer" },
-	automatic_install = true,
-})
