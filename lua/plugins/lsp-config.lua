d3604344bc97795d6f1f30245cc66668f61c0cae
dwerkjem <derekrneilson@gmail.com>
2024-03-23 00:44:08 -0600

    added mason ensure installed function

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,3 +1,43 @@
+-- Function to check if a Mason package is installed
+local function is_package_installed(package_name)
+  local result = vim.fn.system("mason list | grep '" .. package_name .. "'")
+  return result ~= ""
+end
+
+-- Function to install a Mason package
+local function install_package(package_name)
+  vim.fn.system("mason install " .. package_name)
+end
+
+-- Define a list of Mason packages to be installed
+local mason_packages = {
+  "lua-ls",
+  "tsserver",
+  "html",
+  "pyright",
+  "solargraph",
+  "eslint",
+  "prettier",
+  "pylint",
+  "flake8",
+  "lua-language-server",
+
+}
+
+-- Main function to ensure all Mason packages are installed
+local function ensure_packages_installed()
+  for _, package in ipairs(mason_packages) do
+    if not is_package_installed(package) then
+      print("Installing " .. package .. "...")
+      install_package(package)
+    else
+    end
+  end
+end
+
+-- Call the main function to ensure all Mason packages are installed
+ensure_packages_installed()
+
 return {
   {
     "williamboman/mason.nvim",
@@ -18,20 +58,20 @@ return {
     "neovim/nvim-lspconfig",
     lazy = false,
     config = function()
-      local capabilities = require('cmp_nvim_lsp').default_capabilities()
+      local capabilities = require("cmp_nvim_lsp").default_capabilities()
 
       local lspconfig = require("lspconfig")
       lspconfig.tsserver.setup({
-        capabilities = capabilities
+        capabilities = capabilities,
       })
       lspconfig.solargraph.setup({
-        capabilities = capabilities
+        capabilities = capabilities,
       })
       lspconfig.html.setup({
-        capabilities = capabilities
+        capabilities = capabilities,
       })
       lspconfig.lua_ls.setup({
-        capabilities = capabilities
+        capabilities = capabilities,
       })
 
       -- Keymaps
@@ -41,14 +81,13 @@ return {
       vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, {})
 
       -- Diagnostics should appear when cursor hovers over the line
-      vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
-        vim.lsp.diagnostic.on_publish_diagnostics, {
-          underline = true,
-          virtual_text = false,
-          signs = true,
-          update_in_insert = true,
-        }
-      )
+      vim.lsp.handlers["textDocument/publishDiagnostics"] =
+          vim.lsp.with(vim.lsp.diagnostic.on_publish_diagnostics, {
+            underline = true,
+            virtual_text = false,
+            signs = true,
+            update_in_insert = true,
+          })
     end,
   },
 }
